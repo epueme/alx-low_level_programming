67fb223d3067e89f782ab52820e35643e6364540
epueme <epueme@gmail.com>
2021-06-17 16:16:54 -0700

    task c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdin.h>
+
+/**
+ * Main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(Void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0)
+}

2a3fb5b46fdea3a082b38bc787f47e87ad972885
epueme <epueme@gmail.com>
2021-06-18 08:27:06 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
 * main - Entry point
 *
-*Return: Always 0 (Success)
+* Return: Always 0 (Success)
 */
 int main(void)
 {
@@ -14,7 +14,7 @@ char charType;
 
 printf("Size of int: %zu bytes\n", sizeof(intType));
 printf("Size of float: %zu bytes\n", sizeof(floatType));
-print("Size of double: %zu bytes\n", sizeof(doubleType));
-print("Size of char: %zu byts\n", sizeof(charTypes));
-return (0)
+printf("Size of double: %zu bytes\n", sizeof(doubleType));
+printf("Size of char: %zu byts\n", sizeof(charType));
+ return (0);
 }

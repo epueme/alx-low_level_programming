486d05f642714c013ddd2b62ed59d926740f781b
epueme <epueme@gmail.com>
2021-06-20 14:46:51 -0700

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+#include<stdlib.h>
+#include <time.>
 
 /**
-*
 * main - Entry point
 *
-*Return: Always 0 (Success)
+* Return: Always 0 (Success)
 */
 
 int main(void)
 {
 int ch;
-for (ch= 'a'; ch<= 'z'; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
+{
 putchar (ch);
+}
 putchar ('\n');
 return (0);
+
 }
-  

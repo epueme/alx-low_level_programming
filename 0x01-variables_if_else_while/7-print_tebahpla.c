2b0491049e797223b0ab249171d7d9e77defbad4
epueme <epueme@gmail.com>
2021-06-21 03:04:21 -0700

    task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -20,30 +20,4 @@ putchar(ch);
 putchar('\n');
 return (0);
 }
-[10:39 PM, 6/20/2021] Ken ALX: Task 8
-[10:40 PM, 6/20/2021] Ken ALX: #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-
-{
-int i;
-char ch;
-for (i = '0'; i <= '9'; i++)
-{
-putchar(i);
-}
-for (ch = 'a'; ch <= 'f'; ch++)
-{
-putchar(ch);
-}
-putchar('\n');
-return (0);
-}

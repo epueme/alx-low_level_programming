0b687663fc0cfee730066a718da34a473b56782c
epueme <epueme@gmail.com>
2021-06-23 07:15:24 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+void print.alphabet (void)
+{
+  char alph;
+alph = 'a'
+  while (alph <= 'z')
+    {
+     _putchar(alph);
+     alph++;
+    }
+  _putchar('\n');
+return;
+}

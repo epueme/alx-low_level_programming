f3d05ec95a0cd7eac595f4533fc174d79b176c0c
epueme <epueme@gmail.com>
2021-06-24 05:09:37 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,14 +6,12 @@
  * Return: Always 0 (Success)
  */
 void print_alphabet (void)
-int main(void)
 {
-
 int ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
 _putchar (ch);
 }
 _putchar ('\n');
-return (0);
+return;
 }

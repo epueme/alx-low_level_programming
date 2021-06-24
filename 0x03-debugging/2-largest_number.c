c5bd7e930f77e6eefa830f250a96444852fbc041
epueme <epueme@gmail.com>
2021-06-24 14:36:23 -0700

    task2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,14 +16,6 @@ int largest_number(int a, int b, int c)
     {
         largest = a;
     }
-    else if (b < a && a > c)
-    {
-        largest = a;
-    }
-    else
-    {
-        largest = a;
-    }
 
     return (largest);
 }

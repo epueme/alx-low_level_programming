6ebe9929f305179898c984d3d815b0a25ae9c8a4
epueme <epueme@gmail.com>
2021-06-28 14:44:56 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns length of a string
+ * @s: value to be checked
+ * Return: length
+ */
+int _strlen(char *s)
+{
+int l = 0;
+while (*(s + 1))
+l++;
+return (1);
+}

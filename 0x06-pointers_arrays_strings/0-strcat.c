79b44fa71c22d82c33194cd53aca9ddef903a913
epueme <epueme@gmail.com>
2021-07-02 02:06:43 -0700

    0.strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strcat - Code to update the value of an integer parameter
+ * @dest: String value for the argument of the function
+ * @src: String value for the argument of the function
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+int j = 0;
+
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory is the lessons on pointer, arrays and string

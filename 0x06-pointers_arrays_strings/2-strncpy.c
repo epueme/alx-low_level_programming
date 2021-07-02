7204ff7ae6091e20ab291cc7ebccfcdbb060dd85
epueme <epueme@gmail.com>
2021-07-02 02:33:25 -0700

    task 1-4

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strncpy - Code to join copy string on n byte
+ * @dest: String value for the argument of the function
+ * @src: String value for the argument of the function
+ * @n: Int value for the byte of the string
+ * Return: Destination
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+for (i = 0; src[i] != '\0' && i < n; i++)
+dest[i] = src[i];
+while (i < n)
+{
+dest[i++] = '\0';
+}
+return (dest);
+}
